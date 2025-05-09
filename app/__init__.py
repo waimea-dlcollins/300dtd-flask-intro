058c8b09900b4ab0ddd7d1b44bea683303f96c76
DYLANCOLLINS23 <dlcollins@waimea.school.nz>
2025-05-09 15:07:28 +1200

    flask intro

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def home():
+    return render_template("pages/home.jinja")
+
+
+
+@app.get("/test/")
+def test():
+    return "<h1> testing... 1, 2, 3</h1>"
+
+
